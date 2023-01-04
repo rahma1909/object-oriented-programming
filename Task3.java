27b7a03fb3fe2486469335f62f1cc805e91e146f
Rahma_ Abouelfatouh1 <98321437+rahma1909@users.noreply.github.com>
2023-01-04 10:40:51 +0200

    Add files via upload

---
+++ Task3.java
@@ -0,0 +1,18 @@
+
+package task3;
+
+
+public class Task3 {
+
+   
+    public static void main(String[] args) {
+      pointt p1=new pointt();
+      p1.set_r(10);
+     // System.out.println(p1.get_r());
+      circle c1=new circle();
+     
+      
+       
+    }
+    
+}
